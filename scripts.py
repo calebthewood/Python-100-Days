0e3bc49bf0f46c9851a984ef9842edebe7c979c9
calebthewood <calebwood.cs@gmail.com>
2022-06-25 17:48:23 -0700

    Add pizza_order fn

--- scripts.py
+++ scripts.py
@@ -87,3 +87,26 @@ def check_for_leap_year():
     if year % 4 == 0 and year % 100 != 0:
         result = "Leap year."
     print(result)
+
+def order_pizza():
+    """Day 3: Order a Pizza"""
+    print("Welcome to Python Pizza Deliveries!")
+    size = input("What size pizza do you want? S, M, or L ")
+    add_pepperoni = input("Do you want pepperoni? Y or N ")
+    extra_cheese = input("Do you want extra cheese? Y or N ")
+
+    bill = 0
+    if (size == "S"):
+        bill += 15
+    elif size == "M":
+        bill += 20
+    elif size == "L":
+        bill += 25
+    if add_pepperoni == "Y":
+        if size == "S":
+            bill += 2
+        else:
+            bill += 3
+    if extra_cheese == "Y":
+        bill += 1
+    print(f"Your final bill is: ${bill}")
