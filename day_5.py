d4cb3924735d39cabd9eeb70031cbd99c9924736
calebthewood <calebwood.cs@gmail.com>
2022-06-29 16:17:32 -0700

    Add Fizzbuzz

--- day_5.py
+++ day_5.py
@@ -1,5 +1,5 @@
 
-def avg_student_height():
+def avgStudentHeight():
     """
     takes in a sequence of numbers via input
     and returns the average. No len() or sum()
@@ -18,7 +18,7 @@ def avg_student_height():
     avg_height = round(heights_total / length)
     print(avg_height)
 
-def find_high_score():
+def findHighScore():
     """
     Accept a sequence of scores, returns highest score.
     78 65 89 86 55 91 64 89 >>> 91
@@ -35,9 +35,26 @@ def find_high_score():
 
     print(f"The highest score in the class is: {high_score}")
 
-def sum_evens():
+def sumEvens():
     """Sum evens between 0 and 100, no sum()"""
     evens_sum = 0
     for num in range(2, 101, 2):
         evens_sum += num
     print(evens_sum)
+
+def fizzBuzz():
+    """
+    Prints num in range from 1 to 100.
+    if num divisble by 3 print "Fizz"
+    if num divisible by 5 print "Buzz
+    if num divisible bt 15 print "FizzBuzz
+    """
+    for num in range(1,101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 5 == 0:
+            print("Buzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        else:
+            print(str(num))
