2a20b9f059b29d6822f76da4da5753bf2be130f5
calebthewood <calebwood.cs@gmail.com>
2022-06-29 17:20:57 -0700

    Add pswd generator

--- day_5.py
+++ day_5.py
@@ -1,3 +1,4 @@
+import random
 
 def avgStudentHeight():
     """
@@ -58,3 +59,29 @@ def fizzBuzz():
             print("Fizz")
         else:
             print(str(num))
+
+def randomPasswordGenerator():
+    """Generates a random password based on inputs"""
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    print("Welcome to the PyPassword Generator!")
+    nr_letters= int(input("How many letters would you like in your password?\n"))
+    nr_symbols = int(input(f"How many symbols would you like?\n"))
+    nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+    pswd_list = []
+
+    for letter in range(0,nr_letters):
+        pswd_list.append(random.choice(letters))
+
+    for number in range(0, nr_numbers):
+        pswd_list.append(random.choice(numbers))
+
+    for symbol in range(0, nr_symbols):
+        pswd_list.append(random.choice(symbols))
+
+    random.shuffle(pswd_list)
+    password = "".join(pswd_list)
+    print(password)
