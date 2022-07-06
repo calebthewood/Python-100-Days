9004c846a84d5539675f5dd202dcf3e2831fef8e
calebthewood <calebwood.cs@gmail.com>
2022-07-05 23:02:46 -0700

    Update day_8

--- 100_days/day_8.py
+++ 100_days/day_8.py
@@ -10,3 +10,17 @@ def paint_calc(height, width, cover):
     print (f"You\'ll need {cans_needed} cans of paint.")
 
 paint_calc(height=test_h, width=test_w, cover=coverage)
+
+def prime_checker(number):
+    """Checks for prime number, prints result"""
+    count = number - 1
+    msg = "It\'s a prime number."
+    while count > 1:
+        if number % count == 0:
+            msg = "It's not a prime number."
+            break
+        count-=1
+    print(msg)
+
+n = int(input("Check this number: "))
+prime_checker(number=n)
