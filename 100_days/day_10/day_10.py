20ee902bf62e130b81851079350b859ce7c60885
calebthewood <calebwood.cs@gmail.com>
2022-12-02 12:42:00 -0800

    day 10

---
+++ 100_days/day_10/day_10.py
@@ -0,0 +1,29 @@
+
+def format_name(f_name, l_name):
+    return f"{f_name.title()} {l_name.title()}"
+
+
+def is_leap(year):
+  """Accepts year and returns Boolean if """
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
+
+def days_in_month(year=2022, month=2):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if month == 2 and is_leap(year):
+    return 29
+  else:
+    return month_days[month - 1]
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
