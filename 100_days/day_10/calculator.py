7d03c32e3083a6f481b8fe927f0df981883c9011
calebthewood <calebwood.cs@gmail.com>
2022-12-03 16:15:20 -0800

    Adds recursion

--- 100_days/day_10/calculator.py
+++ 100_days/day_10/calculator.py
@@ -18,24 +18,29 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number?: "))
+def calculator():
+    num1 = int(input("What's the first number?: "))
+    num2 = int(input("What's the second number?: "))
 
-for operation in operations:
-    print(operation)
-
-operation_symbol = input("Pick an operation symbol from the line above: ")
-calculation_func = operations[operation_symbol]
-answer = calculation_func(num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
-
-prompt = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ")
-
-while prompt == 'y':
-    operation_symbol = input("Pick an operation: ")
-    num = int(input("What's the next number?: "))
+    for operation in operations: print(operation)
+    operation_symbol = input("Pick an operation symbol from the line above: ")
     calc_func = operations[operation_symbol]
-    prev_answer = answer
-    answer = calc_func(prev_answer, num)
-    print(f"{prev_answer} {operation_symbol} {num} = {answer}")
+    answer = calc_func(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
     prompt = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ")
+
+    while prompt == 'y':
+        operation_symbol = input("Pick an operation: ")
+        num = int(input("What's the next number?: "))
+        calc_func = operations[operation_symbol]
+        prev_answer = answer
+        answer = calc_func(prev_answer, num)
+        print(f"{prev_answer} {operation_symbol} {num} = {answer}")
+
+        prompt = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ")
+
+        if prompt == "n":
+            return calculator()
+
+calculator()
