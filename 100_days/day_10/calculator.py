a3c8bfe20c56292d8ae08ee9dd4239a98e042b4c
calebthewood <calebwood.cs@gmail.com>
2022-12-03 16:08:23 -0800

    Add loop to calc

--- 100_days/day_10/calculator.py
+++ 100_days/day_10/calculator.py
@@ -25,6 +25,17 @@ for operation in operations:
     print(operation)
 
 operation_symbol = input("Pick an operation symbol from the line above: ")
-result = operations[operation_symbol](num1, num2)
+calculation_func = operations[operation_symbol]
+answer = calculation_func(num1, num2)
+print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-print(f"{num1} {operation_symbol} {num2} = {result}")
+prompt = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ")
+
+while prompt == 'y':
+    operation_symbol = input("Pick an operation: ")
+    num = int(input("What's the next number?: "))
+    calc_func = operations[operation_symbol]
+    prev_answer = answer
+    answer = calc_func(prev_answer, num)
+    print(f"{prev_answer} {operation_symbol} {num} = {answer}")
+    prompt = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ")
