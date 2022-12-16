157f55b2abc229a518449a7a1273c6b67c3d44d9
calebthewood <calebwood.cs@gmail.com>
2022-12-16 09:45:44 -0800

    Completes BlackJack

--- 100_days/day_11/day_11.py
+++ 100_days/day_11/day_11.py
@@ -40,7 +40,6 @@ def black_jack():
     computer_hand = []
     start = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
     if start == "n": return
-    start = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
     print(logo)
 
     player_hand.append(draw_card())
@@ -49,29 +48,29 @@ def black_jack():
     print(f"Your cards: {player_hand}, current score: {score(player_hand)}")
     print(f"Computer's first card: {computer_hand}")
 
+    playing = True
     hit = 'y'
-    while score(player_hand) <= 21 and score(computer_hand) <= 21:
-        chance = random.random()
-        p_score = score(player_hand)
-        c_score = score(computer_hand)
-        player_done = false
+
+    while playing:
 
         # Player Turn:
-        if not player_done:
+        if hit == 'y':
             hit = input("Type 'y' to get another card, type 'n' to pass:")
-            if hit == 'n': player_done = true
-            elif hit == "y":
+            if hit == "y":
                 player_hand.append(draw_card())
                 if score(player_hand) > 21:
                     break
         # Computer Turn
-        if c_score <= p_score:
-            if c_score < 10:
+        if score(computer_hand) <= score(player_hand):
+            chance = random.random()
+            if score(computer_hand) < 10:
                 computer_hand.append(draw_card())
-            elif c_score < 12 and chance < 0.5:
+            elif score(computer_hand) < 12 and chance < 0.5:
                 computer_hand.append(draw_card())
-            elif c_score < 17 and chance < 0.1:
+            elif score(computer_hand) < 17 and chance < 0.1:
                 computer_hand.append(draw_card())
+            elif hit == 'n':
+                break
             if score(computer_hand) > 21:
                 break
 
@@ -81,17 +80,11 @@ def black_jack():
 
     print(f"    Your final hand: {player_hand}, final score: {score(player_hand)}")
     print(f"    Computer's final hand: [4, 10, 1, 5], final score: {score(computer_hand)}")
-    if score(player_hand) > score(computer_hand):
+    if score(player_hand) > score(computer_hand) and score(player_hand) <= 21:
         print("You win!")
     else:
         print("You lose!")
 
-
-
-    #if yes....
-    #if no,
-    #   deal to computer, pc needs a brain to decide when to hit/hold
-    # if pc busts
-    print("Computer's final hand: [10, 5, 3], final score: 18")
-    print("You win!")
     black_jack()
+
+black_jack()
